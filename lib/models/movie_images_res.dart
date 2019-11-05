3eb1520fddae854a13ba2d600e5fd59c4f62d29e
Travis Tressler <r.travis.tressler@gmail.com>
2019-11-05 18:49:42 -0500

    Response object created

---
+++ lib/models/movie_images_res.dart
@@ -0,0 +1,41 @@
+
+
+class MovieImagesRes {
+
+  final _id;
+  final List<ImageBackDrops> _imageBackDrops;
+  final List<ImagePosters> _imagePosters;
+
+  MovieImagesRes.fromJson(Map<String, dynamic> parsedJson)
+    : _id = parsedJson['id'],
+      _imageBackDrops = (parsedJson['backdrops'] as List).map((image) => ImageBackDrops.fromJson(image)).toList(),
+      _imagePosters = (parsedJson['posters'] as List).map((image) => ImagePosters.fromJson(image)).toList();
+
+  List<ImagePosters> get imagePosters => _imagePosters;
+
+  List<ImageBackDrops> get imageBackDrops => _imageBackDrops;
+
+  get id => _id;
+
+}
+
+class ImageBackDrops {
+
+  final String _imagePath;
+
+  ImageBackDrops.fromJson(Map<String, dynamic> parsedJson)
+      : _imagePath = parsedJson['file_path'];
+
+  String get imagePath => _imagePath;
+}
+
+class ImagePosters {
+  
+  final String _imagePath;
+
+  ImagePosters.fromJson(Map<String, dynamic> parsedJson)
+    : _imagePath = parsedJson['file_path'];
+
+  String get imagePath => _imagePath;
+
+}
